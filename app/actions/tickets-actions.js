99905137cd8984714f7a329435aa017cf5c44214
Patrick Kim <ptrkkim@gmail.com>
2017-06-25 00:17:30 -0400

    some more advanced flow typing in reducer for practice and setting a template for future reducers

--- app/actions/tickets-actions.js
+++ app/actions/tickets-actions.js
@@ -13,7 +13,11 @@ export type ticketType = {
   codeState?: string
 };
 
-
+// breaking down flow syntax:
+// state can have a selectedTicket property that is either {} or a ticket
+// state is an object that will be used as a map
+//   this map can have numbers as keys (ticket_id is for semantic documentation)
+//   this map can have values of ticketType
 export type ticketsType = {
   selectedTicket: {} | ticketType,
   [ticket_id: string | number]: ticketType
--- app/reducers/tickets-reducer.js
+++ app/reducers/tickets-reducer.js
@@ -2,23 +2,20 @@
 import { ADD_TICKET, SELECT_TICKET, REMOVE_TICKET } from '../actions/tickets-actions';
 import type { ticketType, ticketsType } from '../actions/tickets-actions';
 
-type actionType = {
-  type: string,
-  id?: number,
-  ticket?: ticketType
-};
+type addAction = { type: typeof ADD_TICKET, id: number, ticket: ticketType };
+type selectAction = { type: typeof SELECT_TICKET, id: number };
+type removeAction = { type: typeof REMOVE_TICKET, id: number };
 
-// breaking down flow syntax:
-// state can have a selectedTicket property that is either {} or a ticket
-// state is an object that will be used as a map
-//   this map can have numbers as keys (ticket_id is for semantic documentation)
-//   this map can have values of ticketType
+type Action =
+  | addAction
+  | selectAction
+  | removeAction;
 
 const defaultTickets = {
   selectedTicket: {}
 };
 
-export default function tickets(state: ticketsType = defaultTickets, action: actionType) {
+export default function tickets(state: ticketsType = defaultTickets, action: Action) {
   switch (action.type) {
     case ADD_TICKET:
       return {
