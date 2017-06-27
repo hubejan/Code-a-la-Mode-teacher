360bcb02ab5bf3cc84da4b620599bfd9a7753a31
Patrick Kim <ptrkkim@gmail.com>
2017-06-27 10:52:14 -0400

    test: auth reducer is working as expected, minor fixes to state returned by logout condition

---
+++ test/reducers/auth-reducer.spec.js
@@ -0,0 +1,35 @@
+import authReducer from '../../app/reducers/auth-reducer';
+import { LOGIN_SUCCESS, LOGIN_FAILURE, LOGOUT } from '../../app/actions/auth-actions';
+
+describe('auth reducer', () => {
+  const loggedOutState = {
+    token: null,
+    error: {},
+    failed: false
+  };
+
+  const loggedInState = {
+    token: 'anAccessToken',
+    error: {},
+    failed: false
+  };
+
+  it('should handle initial state', () => {
+    expect(authReducer(undefined, {})).toMatchSnapshot();
+  });
+
+  it('should handle LOGIN_SUCCESS', () => {
+    const successAction = { type: LOGIN_SUCCESS, token: 'anAccessToken' };
+    expect(authReducer(loggedOutState, successAction)).toMatchSnapshot();
+  });
+
+  it('should handle LOGIN_FAILURE', () => {
+    const error = { error: 'bad validation', error_description: 'incorrect code', error_uri: '/error' };
+    const failureAction = { type: LOGIN_FAILURE, error };
+    expect(authReducer(loggedOutState, failureAction)).toMatchSnapshot();
+  });
+
+  it('should handle LOGOUT', () => {
+    expect(authReducer(loggedInState, { type: LOGOUT })).toMatchSnapshot();
+  });
+});
