c9aa553be84f59cae02c337783deeeb616f89602
Patrick Kim <ptrkkim@gmail.com>
2017-06-27 12:32:46 -0400

    tests: ensures async login action is working as expected

---
+++ test/actions/auth-actions.spec.js
@@ -0,0 +1,64 @@
+import * as actions from '../../app/actions/auth-actions';
+
+const validAuthInfo = {
+  client_id: 'a valid id',
+  client_secret: 'a valid secret',
+  code: 'a valid code'
+};
+
+const error = {
+  error: 'bad validation',
+  error_description: 'incorrect code',
+  error_uri: '/error'
+};
+
+const postCredentialsMock = (authInfo) => new Promise((resolve, reject) => {
+  // defer execution of function until next event loop iteration
+  process.nextTick(
+    () => (
+      validAuthInfo.client_id === authInfo.client_id &&
+      validAuthInfo.client_secret === authInfo.client_secret &&
+      validAuthInfo.code === authInfo.code
+      ? resolve({ access_token: 'a valid access token' })
+      : reject(error))
+  );
+});
+
+const teacherLoginMock = (authCode: string) => (dispatch: *) => {
+  const authInfo = {
+    client_id: 'a valid id',
+    client_secret: 'a valid secret',
+    code: authCode
+  };
+
+  return postCredentialsMock(authInfo)
+    .then(response =>
+      dispatch({ type: actions.LOGIN_SUCCESS, token: response.access_token })
+    )
+    .catch(authError =>
+      dispatch({ type: actions.LOGIN_FAILURE, error: authError })
+    );
+};
+
+const mockedTeacherLogin = jest.fn(teacherLoginMock);
+describe('action creators', () => {
+  it('should create a LOGOUT action', () => {
+    expect(actions.logout()).toMatchSnapshot();
+  });
+
+  it('should create an action with a valid token on successful login', () => {
+    const dispatcherFunc = mockedTeacherLogin('a valid code');
+    const mockDispatch = jest.fn();
+    return dispatcherFunc(mockDispatch)
+    .then(() =>
+      expect(mockDispatch).toBeCalledWith({ type: 'LOGIN_SUCCESS', token: 'a valid access token' }));
+  });
+
+  it('should create an action with an error object on failed login', () => {
+    const dispatcherFunc = mockedTeacherLogin('INVALID CODE');
+    const mockDispatch = jest.fn();
+    return dispatcherFunc(mockDispatch)
+    .then(() =>
+      expect(mockDispatch).toBeCalledWith({ type: 'LOGIN_FAILURE', error }));
+  });
+});
