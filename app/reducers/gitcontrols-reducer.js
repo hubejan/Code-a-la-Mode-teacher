b3423a51f5771f8467c9481640bff19dcb2cbc73
brianhong <brianhong0510@gmail.com>
2017-06-27 13:54:25 -0400

    Integrated Nodegit to our project.

---
+++ app/reducers/gitcontrols-reducer.js
@@ -0,0 +1,26 @@
+// @flow
+// import { GOT_USERNAME } from '../actions/filetree-actions';
+
+// export type filetreeStateType = {
+//   username: string
+// };
+
+// type actionType = {
+//   type: string,
+//   username?: string
+// };
+
+// const defaultFiletreeState = {
+//   username: ''
+// };
+
+// export default function filetree(state: filetreeStateType = defaultFiletreeState,
+//   action: actionType) {
+//   switch (action.type) {
+//     case GOT_USERNAME:
+//       return { ...state, username: action.username };
+
+//     default:
+//       return state;
+//   }
+// }
