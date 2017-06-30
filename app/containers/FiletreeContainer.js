c8455b0d6c026ba1327b1362fb1182339eb57637
brianhong <brianhong0510@gmail.com>
2017-06-30 17:55:21 -0400

    Beginning work on saving changes to project from instructor's side

--- app/containers/FiletreeContainer.js
+++ app/containers/FiletreeContainer.js
@@ -1,7 +1,6 @@
 import { connect } from 'react-redux';
 import Filetree from '../components/Filetree';
 import * as FiletreeActions from '../actions/filetree-actions';
-import * as UserRepositoriesActions from '../actions/userrepositories-actions';
 
 function mapStateToProps(state) {
   return {
